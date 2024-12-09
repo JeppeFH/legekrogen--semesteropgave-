26b334e2e01701b584f769d4c421e40a5507af3f
JeppeFH <jeppefinch@hotmail.dk>
2024-12-09 14:03:59 +0100

    footer changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,31 @@
 import styles from "./footer.module.css";
+import { CiMail } from "react-icons/ci";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { LuInstagram } from "react-icons/lu";
 
 const Footer = () => {
   return (
     <footer>
-      <h3>Kundeservice</h3>
+      <div className="customerService">
+        <h3>Kundeservice</h3>{" "}
+        <figure>
+          <CiMail size={20} color="white" />
+          <p>kontakt@legekrogen.dk</p>
+        </figure>
+        <figure>
+          <FaPhoneAlt size={20} color="white" />
+          <p>+45 23 45 67 89</p>
+        </figure>
+      </div>
+
+      <div className="followUs">
+        <h3>Følg os</h3>
+        <figure>
+          <FaFacebook size={30} color="white" />
+          <LuInstagram size={30} color="white" />
+        </figure>
+      </div>
     </footer>
   );
 };
