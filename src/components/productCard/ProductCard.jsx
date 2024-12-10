30edb45bc3fba0f53352cf9f2c4b1bf5550340a6
JeppeFH <jeppefinch@hotmail.dk>
2024-12-10 13:17:37 +0100

    css shipping and productcard

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -5,14 +5,16 @@ const ProductCard = ({ product }) => {
   return (
     <figure className={styles.productCard}>
       <div className={styles.imageContainer}>
-        <Link to={`/products/${product.id}`}>
+        <Link to={`/products/${product._id}`}>
           <img src={product.image} alt={product.title} />
         </Link>
       </div>
 
-      <h4>{product.title}</h4>
-      <h5>{product.description}</h5>
-      <p>{product.price} kr</p>
+      <div className={styles.productCardInfo}>
+        <h4>{product.title}</h4>
+        <h5>{product.description}</h5>
+        <p>{product.price} kr</p>
+      </div>
     </figure>
   );
 };
